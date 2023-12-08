ac5992c9f80238aea2332785ed86e01940864c72
mikaeel khalid <mikaeelkhalid@icloud.com>
2023-12-08 21:03:17 +0100

    zip lambda function

--- main.tf
+++ main.tf
@@ -7,3 +7,12 @@ resource "null_resource" "install_lambda_function_packages" {
     command = "cd ${path.module}/aws-codepipeline-discord-lambda && npm install"
   }
 }
+
+# zip lambda function
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/aws-codepipeline-discord-lambda"
+  output_path = "${path.module}/tmp/aws-codepipeline-discord-lambda.zip"
+
+  depends_on = [null_resource.install_lambda_function_packages]
+}
